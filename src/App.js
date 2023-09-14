34e68addc2b034d7ea42089245c16b6445d1a413
Anwesh Mishra <19btcse07@suiit.ac.in>
2023-09-14 23:43:48 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import DownloadMessage from "./Component/DownloadMessage";
 import Footer from "./Component/Footer";
-import Hamburger from "./Component/HamburgerMenu/Hamburger";
+import Hamburger from "./Component/Hamburger";
 import Header from "./Component/Header";
 import NewsContent from "./Component/NewsContent";
 
 function App() {
-  const key = `49d1d651d12d465eaf4f9d22ba825954`
-  // const YOUR_API_KEY = `pub_133669758107c20713122e5549c5d48621d00`
+  const key = process.env.REACT_APP_NEWS_API_KEY;
   const [category, setCategory] = useState("general");
-  const [newsArray, setNewsArray] = useState([])
+  const [newsArray, setNewsArray] = useState([]);
   const [newsResult, setNewsResult] = useState();
-  const [loadMore, setLoadMore] = useState(10);
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   const newsApi = async () => {
-    // e.preventDefault();
-    const news = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${key}`)
-    // const news = await fetch(`https://saurav.tech/NewsAPI/top-headlines/category/${category}/in.json`)
-    // const news = await fetch(`https://newsdata.io/api/1/news?apikey=${YOUR_API_KEY}&category=${category}&country=in&language=en`)
-    const data = await news.json()
+    const news = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${key}`
+    );
+    const data = await news.json();
     setNewsArray(data.articles);
-    setNewsResult(data.totalResults)
-  }
+    setNewsResult(data.totalResults);
+  };
   // newsApi();
   useEffect(() => {
     newsApi();
-  }, [newsResult, category, loadMore]);
+  }, [newsResult, category]);
   return (
     <>
       <div className="flex flex-col items-center">
         <Header navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
-        <Hamburger setCategory={setCategory} navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
+        <Hamburger
+          setCategory={setCategory}
+          navbarOpen={navbarOpen}
+          setNavbarOpen={setNavbarOpen}
+        />
         <DownloadMessage />
-        <NewsContent 
-          setLoadMore={setLoadMore}
-          loadMore={loadMore}
-          newsArray={newsArray}
-          newsResult={newsResult} />
+        <NewsContent newsArray={newsArray} />
         <Footer />
       </div>
     </>
--- src/Component/NewsContent.jsx
+++ src/Component/NewsContent.jsx
@@ -1,25 +1,13 @@
 import React from 'react'
 import NewsCard from './NewsCard'
 
-function NewsContent({ newsArray, loadMore, setLoadMore, newsResult }) {
-  const handleClick = () => {
-    console.log("Clicked")
-    setLoadMore(loadMore + 20)
-  }
+function NewsContent({ newsArray }) {
   return (
     <>
       <div className='flex flex-col'>
-        {newsArray.slice(0, loadMore).map((newsItem, i) => (
+        {newsArray.map((newsItem, i) => (
           <NewsCard newsItem={newsItem} key={newsItem.title} />
         ))}
-        {loadMore <= newsResult && (
-          <div className='flex'>
-            <div className='flex items-center mx-auto'>
-              <hr />
-              <button onClick={handleClick} className='mt-8 rounded-3xl shadow-xl shadow-gray-600 bg-red-600 px-5 py-2 text-white text-md font-bold'>Load More</button>
-            </div>
-          </div>
-        )}
       </div>
     </>
   )
